567253de948e2afc7053959c02f499caa305113e
Anudi Mudara <anudimudara@gmail.com>
2024-02-03 10:40:32 +0530

    Add, manage, update done
    Add product, manage product, update product

---
+++ designx/src/screens/UpdateProduct.js
@@ -0,0 +1,14 @@
+import { DataGrid, GridColDef, GridRowsProp } from '@mui/x-data-grid';
+import React from 'react'
+import Grid from '../../components/Grid';
+
+function UpdateProduct() {
+  return (
+    <div>
+        <h1 className='heading-text'>Update Products</h1>
+        <Grid/>
+    </div>
+  )
+}
+
+export default UpdateProduct
